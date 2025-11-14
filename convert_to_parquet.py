8effa2147006c3f7c97dcd29d29fdf9f58e2bc73
Nandu Panakanti <150353077+panakantinandu@users.noreply.github.com>
2025-11-14 03:52:48 -0600

    Delete convert_to_parquet.py

--- convert_to_parquet.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-print("Loading 245MB CSV…")
-df = pd.read_csv("HR_Data.csv")
-
-print("Saving compressed Parquet…")
-df.to_parquet("HR_Data.parquet", compression="snappy")
-
-print("Done! HR_Data.parquet created successfully.")
